f10e092c029d60dae4fc8c6791076dbd01d1236c
Abhishek Yadav <abhishekpsitfor4years@gmail.com>
2023-10-19 23:27:14 +0530

    code updated

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -60,7 +60,6 @@ export const Sidebar = () => {
   const [active, setActive] = useState("dashboard");
   const isSmallScreen = useMediaQuery("(max-width: 768px)");
 
-  // State for controlling the Drawer on small screens
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const toggleDrawer = () => {
@@ -79,7 +78,6 @@ export const Sidebar = () => {
         </IconButton>
       ) : null}
 
-      {/* Drawer for small screens */}
       <Drawer
         anchor="left"
         open={drawerOpen}
@@ -136,101 +134,106 @@ export const Sidebar = () => {
 
       {/* Fixed sidebar for large screens */}
       {!isSmallScreen ? (
-        <Box
-          maxWidth="280px"
-          height="100vh"
-          m="1.5rem 2rem 2rem 3rem"
-          sx={{ background: "#040440", color: "white" }}
-        >
-          <FlexBetween>
+        <Box>
+          <Box m="1.5rem 2rem 2rem 3rem">
+            {/* <FlexBetween> */}
             <Box
               display="flex"
               alignItems="center"
-              flexDirection="row"
+              // flexDirection={"row"}
               gap="0.5rem"
-              justifyContent="space-between"
+              justifyContent={"space-between"}
+              color={"white"}
             >
               <DashboardOutlined />
               <Typography variant="h5" fontWeight="bold">
                 DASHBOARD
               </Typography>
             </Box>
-          </FlexBetween>
-          <List sx={{ color: "#61658B" }}>
-            {navItems.map(({ text, icon, arrow }) => {
-              if (!icon) {
+            {/* </FlexBetween> */}
+            {/* </Box> */}
+            <List sx={{ color: "#61658B" }}>
+              {navItems.map(({ text, icon, arrow }) => {
+                if (!icon) {
+                  return (
+                    <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
+                      {text}
+                    </Typography>
+                  );
+                }
+                const lcText = text.toLocaleLowerCase();
                 return (
-                  <Typography key={text} sx={{ m: "2.25rem 0 1rem 3rem" }}>
-                    {text}
-                  </Typography>
-                );
-              }
-              const lcText = text.toLocaleLowerCase();
-
-              return (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton
-                    onClick={() => {
-                      navigate(`/${lcText}`);
-                      setActive(lcText);
-                    }}
-                    sx={{
-                      marginRight: "10px",
-                      marginLeft: "10px",
-                      borderRadius: ".6rem",
-                      width: "50%",
-                      backgroundColor:
-                        active === lcText ? "#2D2D69" : "transparent",
-                      color: active === lcText ? "white" : "#61658B",
-                    }}
-                  >
-                    <ListItemIcon
+                  <ListItem key={text} disablePadding>
+                    <ListItemButton
+                      onClick={() => {
+                        navigate(`/${lcText}`);
+                        setActive(lcText);
+                      }}
                       sx={{
+                        // marginRight: "10px",
+                        // marginLeft: "10px",
+                        borderRadius: ".6rem",
+                        width: "50%",
+                        backgroundColor:
+                          active === lcText ? "#2D2D69" : "transparent",
                         color: active === lcText ? "white" : "#61658B",
                       }}
                     >
-                      {icon}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                    {arrow}
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-          <Box
-            sx={{
-              background: "#2D2D69",
-              borderRadius: "5px",
-              margin: "10px",
-            }}
-          >
-            <FlexBetween textTransform="none" gap="5px" padding="10px">
-              <Box
-                component="img"
-                alt="profile"
-                src="https://t4.ftcdn.net/jpg/02/45/56/35/360_F_245563558_XH9Pe5LJI2kr7VQuzQKAjAbz9PAyejG1.jpg"
-                height="40px"
-                width="40px"
-                borderRadius="50%"
-                sx={{ objectFit: "cover" }}
-              />
-              <Box textAlign="left">
-                <Typography
-                  fontWeight="bold"
-                  fontSize="0.9rem"
-                  sx={{ color: "white" }}
-                >
-                  Shrukh
-                </Typography>
-                <Typography fontSize="0.8rem" sx={{ color: "gray" }}>
-                  Project Manager
-                </Typography>
-              </Box>
-              <KeyboardArrowDownOutlined
-                sx={{ color: "white", fontSize: "25px" }}
-              />
-            </FlexBetween>
+                      <ListItemIcon
+                        sx={{
+                          // ml: "1rem",
+                          color: active === lcText ? "white" : "#61658B ",
+                        }}
+                      >
+                        {icon}
+                      </ListItemIcon>
+                      <ListItemText primary={text} />
+                      {arrow}
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+            <Box
+              position="absolute"
+              bottom="2rem"
+              sx={{
+                background: "#2D2D69",
+                borderRadius: "5px",
+
+                // margin: "10px",
+                // marginBottom: "15px",
+              }}
+            >
+              <FlexBetween textTransform="none" gap="5px" padding={"10px"}>
+                <Box
+                  component="img"
+                  alt="profile"
+                  src={
+                    "https://t4.ftcdn.net/jpg/02/45/56/35/360_F_245563558_XH9Pe5LJI2kr7VQuzQKAjAbz9PAyejG1.jpg"
+                  }
+                  height="40px"
+                  width="40px"
+                  borderRadius="50%"
+                  sx={{ objectFit: "cover" }}
+                />
+                <Box textAlign="left">
+                  <Typography
+                    fontWeight="bold"
+                    fontSize="0.9rem"
+                    sx={{ color: "white" }}
+                  >
+                    Shrukh
+                  </Typography>
+                  <Typography fontSize="0.8rem" sx={{ color: "gray" }}>
+                    Project Manager
+                  </Typography>
+                </Box>
+                <KeyboardArrowDownOutlined
+                  sx={{ color: "white", fontSize: "25px" }}
+                />
+              </FlexBetween>
+            </Box>
           </Box>
         </Box>
       ) : null}
